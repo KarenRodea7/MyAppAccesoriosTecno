2df3b887696d4cdc3cf0f6f51ce69721408e2ef6
Karitz <krnrodea@gmail.com>
2024-04-17 09:32:56 -0600

    Octavo commit

--- app/src/main/java/com/example/accesorioselect/Descripcion.java
+++ app/src/main/java/com/example/accesorioselect/Descripcion.java
@@ -45,50 +45,68 @@ public class Descripcion extends AppCompatActivity {
                 ima.setImageResource(R.drawable.img_3);
                 des.setText("Funda de componentes\n\n" + "Funda de componentes de celular para iPhone 14 Pro, Zero One.\n" + "Color: Negro\n" + "$200");
                 prec=200;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
             else if(valP==2){
                 ima.setImageResource(R.drawable.img_4);
                 des.setText("Funda de mariposas\n\n"+"Funda de teléfono con mariposa de onda brillante y pulsera para iPhone.\n" + "Color: Morado\n"+"$120");
                 prec=120;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
             else if(valP==3){
                 ima.setImageResource(R.drawable.img_5);
                 des.setText("Funda de astronauta\n\n"+"Funda para teléfono Motorola con soporte de Astronauta y correa.\n" + "Color: Negro\n$160");
                 prec=160;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
         }
         else if(val==2){
             if(valP==1) {
                 ima.setImageResource(R.drawable.img_6);
-                des.setText("Audifonos de gato negro\n\n" + "Audifonos bluetooth inalambricos de diadema con radio FM, diseño de Gato negro.\n" + "Color: Negro\n$270");
+                des.setText("Audifonos de gato negro\n\n" + "Audifonos bluetooth inalambricos de diadema con radio FM.\n" + "Color: Negro\n$270");
                 prec=270;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
             else if(valP==2){
                 ima.setImageResource(R.drawable.img_7);
-                des.setText("Audifonos deportivos\n\n"+"Audífonos TWS inalámbricos deportivos con micrófono, intrauditivos con cancelación de ruido y manos libres.\n" + "Color: Multicolor\n$100");
+                des.setText("Audifonos deportivos\n\n"+"Audífonos inalámbricos deportivos con micrófono y con cancelación de ruido y manos libres.\n" + "Color: Multicolor\n$100");
                 prec=100;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
             else if(valP==3){
                 ima.setImageResource(R.drawable.img_8);
-                des.setText("Audifonos con banda para cuello\n\n"+"Audífonos inalámbricos intraauriculares con banda para el cuello y micrófono para llamadas con manos libres.\n" + "Color: Negro\n$170");
+                des.setText("Audifonos con banda para cuello\n\n"+"Audífonos inalámbricos intraauriculares con banda para el cuello y micrófono.\n" + "Color: Negro\n$170");
                 prec=170;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
         }
         else if(val==3){
             if(valP==1) {
                 ima.setImageResource(R.drawable.img);
-                des.setText("Teclado flexible\n\n" + "Teclado flexible y plegable de Silicona resistente al agua para juegos USB mudo con 85 teclas. \n" + "Color: Negro\n$210");
+                des.setText("Teclado flexible\n\n" + "Teclado flexible y plegable de Silicona resistente al agua para juegos USB mudo. \n" + "Color: Negro\n$210");
                 prec=210;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
             else if(valP==2){
                 ima.setImageResource(R.drawable.img_1);
                 des.setText("Teclado mini\n\n"+"Teclado mini con retroiluminación de 7 colores y touchpad con efecto de marquesina. \n" + "Color: Negro\n$160");
                 prec=160;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
             else if(valP==3){
                 ima.setImageResource(R.drawable.img_2);
                 des.setText("Teclado inalámbrico\n\n"+"Teclado bluetooth inalámbrico con Touchpad integrado, multi-dispositivo con soporte para TV, teléfono tableta.\n" + "Color: Negro\n$300");
                 prec=300;
+                sum1=0;
+                cant.setText(String.valueOf(sum1));
             }
         }
 
