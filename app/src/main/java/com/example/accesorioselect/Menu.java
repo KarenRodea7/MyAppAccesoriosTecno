4ef352754bf325b1c2f3481366119a2504f2b016
Karitz <krnrodea@gmail.com>
2024-04-16 20:56:50 -0600

    Segundo commit

--- app/src/main/java/com/example/accesorioselect/Menu.java
+++ app/src/main/java/com/example/accesorioselect/Menu.java
@@ -2,13 +2,54 @@ package com.example.accesorioselect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Menu extends AppCompatActivity {
+
+    ImageButton Fundas, Audif, Otro;
+
+    int val;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Fundas = findViewById(R.id.Fundas);
+        Audif = findViewById(R.id.Audif);
+        Otro = findViewById(R.id.Otro);
+
+        Fundas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Productos.class);
+                val = 1;
+                startActivity(i);
+            }
+        });
+
+        Audif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Productos.class);
+                val = 2;
+                startActivity(i);
+            }
+        });
+
+        Otro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Productos.class);
+                val = 3;
+                startActivity(i);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/accesorioselect/Productos.java
+++ app/src/main/java/com/example/accesorioselect/Productos.java
@@ -2,13 +2,56 @@ package com.example.accesorioselect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Productos extends AppCompatActivity {
+
+    ImageButton P1, P2, P3;
+    int valP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+
+        P1 = findViewById(R.id.Prod1);
+        P2 = findViewById(R.id.Prod2);
+        P3 = findViewById(R.id.Prod3);
+
+
+
+
+        P1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Descripcion.class);
+                valP = 1;
+                startActivity(i);
+            }
+        });
+
+        P2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Descripcion.class);
+                valP = 2;
+                startActivity(i);
+            }
+        });
+
+        P3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Descripcion.class);
+                valP = 3;
+                startActivity(i);
+            }
+        });
+
+
     }
 }
